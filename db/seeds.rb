3d0cc49c7f91ef283b3d3b91114fb341ff1fa95b
jay johnson <jayj10080@gmail.com>
2018-03-11 01:31:11 +0000

    Added routes index faker gem, seeded db with dummy data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+1000.times do 
+  Contact.create([{
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number         
+    }])
+end
